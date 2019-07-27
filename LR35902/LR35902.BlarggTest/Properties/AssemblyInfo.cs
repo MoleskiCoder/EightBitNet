24934729c25c779f3631a757952594ad03fdaf0e
Adrian Conlon <Adrian.conlon@gmail.com>
2019-07-27 19:30:56 +0100

    Fix stylecop warning in LR35902.
    Signed-off-by: Adrian Conlon <Adrian.conlon@gmail.com>

--- LR35902/LR35902.BlarggTest/Properties/AssemblyInfo.cs
+++ LR35902/LR35902.BlarggTest/Properties/AssemblyInfo.cs
@@ -1,4 +1,8 @@
-﻿using System.Reflection;
+﻿// <copyright file="AssemblyInfo.cs" company="Adrian Conlon">
+// Copyright (c) Adrian Conlon. All rights reserved.
+// </copyright>
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
