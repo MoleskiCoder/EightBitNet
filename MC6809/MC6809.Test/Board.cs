5026c275824baf321895cb876f61fb2951157a52
Adrian Conlon <adrian.conlon@gmail.com>
2026-05-09 18:23:13 +0100

    Correct performance issue in "board" by fixing uncontrolled MemoryMapping allocations.

--- MC6809/MC6809.Test/Board.cs
+++ MC6809/MC6809.Test/Board.cs
@@ -7,30 +7,39 @@ namespace MC6809.Test
     using EightBit;
     using System;
 
-    public sealed class Board : Bus
+    internal sealed class Board : Bus
     {
-        private readonly Configuration configuration;
-        private readonly Ram ram = new(0x8000);                                 // 0000 - 7FFF, 32K RAM
-        private readonly UnusedMemory unused2000 = new(0x2000, 0xff);  // 8000 - 9FFF, 8K unused
-        private readonly Ram io = new(0x2000);                                  // A000 - BFFF, 8K serial interface, minimally decoded
-        private readonly Rom rom = new(0x4000);                                 // C000 - FFFF, 16K ROM
+        private readonly Configuration _configuration;
+        private readonly Ram _ram = new(0x8000);                        // 0000 - 7FFF, 32K RAM
+        private readonly UnusedMemory _unused2000 = new(0x2000, 0xff);  // 8000 - 9FFF, 8K unused
+        private readonly Ram _io = new(0x2000);                         // A000 - BFFF, 8K serial interface, minimally decoded
+        private readonly Rom _rom = new(0x4000);                        // C000 - FFFF, 16K ROM
 
-        private readonly Disassembler disassembler;
-        private readonly Profiler profiler;
+        private readonly MemoryMapping _ramMapping;
+        private readonly MemoryMapping _unusedMapping;
+        private readonly MemoryMapping _ioMapping;
+        private readonly MemoryMapping _romMapping;
 
-        private ulong totalCycleCount = 0UL;
-        private long frameCycleCount = 0L;
+        private readonly Disassembler _disassembler;
+        private readonly Profiler _profiler;
 
-        // The m_disassembleAt and m_ignoreDisassembly are used to skip pin events
-        private ushort disassembleAt = 0x0000;
-        private bool ignoreDisassembly = false;
+        private ulong _totalCycleCount;
+        private long _frameCycleCount;
+
+        // The _disassembleAt and _ignoreDisassembly are used to skip pin events
+        private ushort _disassembleAt;
+        private bool _ignoreDisassembly;
 
         public Board(Configuration configuration)
         {
-            this.configuration = configuration;
-            CPU = new MC6809(this);
-            disassembler = new Disassembler(this, CPU);
-            profiler = new Profiler(this, CPU, disassembler);
+            this._configuration = configuration;
+            this.CPU = new MC6809(this);
+            this._disassembler = new Disassembler(this, this.CPU);
+            this._profiler = new Profiler(this, this.CPU, this._disassembler);
+            this._ramMapping = new MemoryMapping(this._ram, 0x0000, Mask.Sixteen, AccessLevel.ReadWrite);
+            this._unusedMapping = new MemoryMapping(this._unused2000, 0x8000, Mask.Sixteen, AccessLevel.ReadOnly);
+            this._ioMapping = new MemoryMapping(this._io, 0xa000, Mask.Sixteen, AccessLevel.ReadWrite);
+            this._romMapping = new MemoryMapping(this._rom, 0xc000, Mask.Sixteen, AccessLevel.ReadOnly);
         }
 
         public MC6809 CPU { get; }
@@ -41,20 +50,20 @@ namespace MC6809.Test
         {
             if (absolute < 0x8000)
             {
-                return new MemoryMapping(ram, 0x0000, Mask.Sixteen, AccessLevel.ReadWrite);
+                return this._ramMapping;
             }
 
             if (absolute < 0xa000)
             {
-                return new MemoryMapping(unused2000, 0x8000, Mask.Sixteen, AccessLevel.ReadOnly);
+                return this._unusedMapping;
             }
 
             if (absolute < 0xc000)
             {
-                return new MemoryMapping(io, 0xa000, Mask.Sixteen, AccessLevel.ReadWrite);
+                return this._ioMapping;
             }
 
-            return new MemoryMapping(rom, 0xc000, Mask.Sixteen, AccessLevel.ReadOnly);
+            return this._romMapping;
         }
 
         public override void RaisePOWER()
@@ -62,29 +71,29 @@ namespace MC6809.Test
             base.RaisePOWER();
 
             // Get the CPU ready for action
-            CPU.RaisePOWER();
-            CPU.LowerRESET();
-            CPU.RaiseINT();
-            CPU.RaiseNMI();
-            CPU.RaiseFIRQ();
-            CPU.RaiseHALT();
+            this.CPU.RaisePOWER();
+            this.CPU.LowerRESET();
+            this.CPU.RaiseINT();
+            this.CPU.RaiseNMI();
+            this.CPU.RaiseFIRQ();
+            this.CPU.RaiseHALT();
 
             // Get the ACIA ready for action
             Address.Word = 0b1010000000000000;
             Data = (byte)(MC6850.ControlRegister.CR0 | MC6850.ControlRegister.CR1);  // Master reset
-            ACIA.CTS.Lower();
-            ACIA.RW.Lower();
-            UpdateAciaPins();
-            ACIA.RaisePOWER();
-            AccessAcia();
+            this.ACIA.CTS.Lower();
+            this.ACIA.RW.Lower();
+            this.UpdateAciaPins();
+            this.ACIA.RaisePOWER();
+            this.AccessAcia();
         }
 
         public override void LowerPOWER()
         {
             ////this.profiler.Generate();
 
-            ACIA.LowerPOWER();
-            CPU.LowerPOWER();
+            this.ACIA.LowerPOWER();
+            this.CPU.LowerPOWER();
             base.LowerPOWER();
         }
 
@@ -93,14 +102,14 @@ namespace MC6809.Test
             Console.TreatControlCAsInput = true;
 
             // Load our BASIC interpreter
-            var directory = configuration.RomDirectory + "\\";
+            var directory = this._configuration.RomDirectory + "\\";
             LoadHexFile(directory + "ExBasROM.hex");
 
             // Catch a byte being transmitted
-            ACIA.Transmitting += ACIA_Transmitting;
+            this.ACIA.Transmitting += ACIA_Transmitting;
 
             // Keyboard wiring, check for input once per frame
-            CPU.ExecutedInstruction += CPU_ExecutedInstruction;
+            this.CPU.ExecutedInstruction += CPU_ExecutedInstruction;
 
             // Marshal data from ACIA -> memory
             this.ReadingByte += Board_ReadingByte;
@@ -108,17 +117,17 @@ namespace MC6809.Test
             // Marshal data from memory -> ACIA
             this.WrittenByte += Board_WrittenByte;
 
-            if (configuration.DebugMode)
+            if (this._configuration.DebugMode)
             {
                 // MC6809 disassembly wiring
-                CPU.ExecutingInstruction += CPU_ExecutingInstruction;
-                CPU.ExecutedInstruction += CPU_ExecutedInstruction_Debug;
+                this.CPU.ExecutingInstruction += CPU_ExecutingInstruction;
+                this.CPU.ExecutedInstruction += CPU_ExecutedInstruction_Debug;
             }
 
-            if (configuration.TerminatesEarly)
+            if (this._configuration.TerminatesEarly)
             {
                 // Early termination condition for CPU timing code
-                CPU.ExecutedInstruction += CPU_ExecutedInstruction_Termination;
+                this.CPU.ExecutedInstruction += CPU_ExecutedInstruction_Termination;
             }
 
             ////this.profiler.Enable();
@@ -127,21 +136,21 @@ namespace MC6809.Test
 
         private void Board_ReadingByte(object? sender, EventArgs e)
         {
-            UpdateAciaPins();
-            ACIA.RW.Raise();
-            if (AccessAcia())
+            this.UpdateAciaPins();
+            this.ACIA.RW.Raise();
+            if (this.AccessAcia())
             {
-                Poke(ACIA.DATA);
+                this.Poke(this.ACIA.DATA);
             }
         }
 
         private void Board_WrittenByte(object? sender, EventArgs e)
         {
-            UpdateAciaPins();
-            if (ACIA.Selected)
+            this.UpdateAciaPins();
+            if (this.ACIA.Selected)
             {
-                ACIA.RW.Lower();
-                AccessAcia();
+                this.ACIA.RW.Lower();
+                this.AccessAcia();
             }
         }
 
@@ -154,75 +163,75 @@ namespace MC6809.Test
 
         private void CPU_ExecutedInstruction_Termination(object? sender, EventArgs e)
         {
-            totalCycleCount += (ulong)CPU.Cycles;
-            if (totalCycleCount > Configuration.TerminationCycles)
+            this._totalCycleCount += (ulong)this.CPU.Cycles;
+            if (this._totalCycleCount > Configuration.TerminationCycles)
             {
-                LowerPOWER();
+                this.LowerPOWER();
             }
         }
 
         private void CPU_ExecutedInstruction_Debug(object? sender, EventArgs e)
         {
-            if (!ignoreDisassembly)
+            if (!this._ignoreDisassembly)
             {
-                var disassembled = $"{disassembler.Trace(disassembleAt)}\t{ACIA.DumpStatus()}";
+                var disassembled = $"{this._disassembler.Trace(this._disassembleAt)}\t{this.ACIA.DumpStatus()}";
                 Console.Error.WriteLine(disassembled);
             }
         }
 
         private void CPU_ExecutingInstruction(object? sender, EventArgs e)
         {
-            disassembleAt = CPU.PC.Word;
-            ignoreDisassembly = disassembler.Ignore || disassembler.Pause;
+            this._disassembleAt = this.CPU.PC.Word;
+            this._ignoreDisassembly = this._disassembler.Ignore || this._disassembler.Pause;
         }
 
         private void CPU_ExecutedInstruction(object? sender, EventArgs e)
         {
-            frameCycleCount -= CPU.Cycles;
-            if (frameCycleCount < 0)
+            this._frameCycleCount -= this.CPU.Cycles;
+            if (this._frameCycleCount < 0)
             {
                 if (Console.KeyAvailable)
                 {
                     var key = Console.ReadKey(true);
                     if (key.Key == ConsoleKey.F12)
                     {
-                        LowerPOWER();
+                        this.LowerPOWER();
                     }
 
-                    ACIA.RDR = Convert.ToByte(key.KeyChar);
-                    ACIA.MarkReceiveStarting();
+                    this.ACIA.RDR = Convert.ToByte(key.KeyChar);
+                    this.ACIA.MarkReceiveStarting();
                 }
 
-                frameCycleCount = (long)Configuration.FrameCycleInterval;
+                this._frameCycleCount = (long)Configuration.FrameCycleInterval;
             }
         }
 
         private void ACIA_Transmitting(object? sender, EventArgs e)
         {
-            Console.Out.Write(Convert.ToChar(ACIA.TDR));
-            ACIA.MarkTransmitComplete();
+            Console.Out.Write(Convert.ToChar(this.ACIA.TDR));
+            this.ACIA.MarkTransmitComplete();
         }
 
         private void UpdateAciaPins()
         {
-            ACIA.DATA = Data;
-            ACIA.RS.Match(Address.Word & (ushort)Bits.Bit0);
-            ACIA.CS0.Match(Address.Word & (ushort)Bits.Bit15);
-            ACIA.CS1.Match(Address.Word & (ushort)Bits.Bit13);
-            ACIA.CS2.Match(Address.Word & (ushort)Bits.Bit14);
+            this.ACIA.DATA = this.Data;
+            this.ACIA.RS.Match(this.Address.Word & (ushort)Bits.Bit0);
+            this.ACIA.CS0.Match(this.Address.Word & (ushort)Bits.Bit15);
+            this.ACIA.CS1.Match(this.Address.Word & (ushort)Bits.Bit13);
+            this.ACIA.CS2.Match(this.Address.Word & (ushort)Bits.Bit14);
         }
 
         private bool AccessAcia()
         {
-            ACIA.E.Raise();
+            this.ACIA.E.Raise();
             try
             {
-                ACIA.Tick();
-                return ACIA.Activated;
+                this.ACIA.Tick();
+                return this.ACIA.Activated;
             }
             finally
             {
-                ACIA.E.Lower();
+                this.ACIA.E.Lower();
             }
         }
     }
