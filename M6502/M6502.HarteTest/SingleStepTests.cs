d8181f51010c1fbee08c68f52540c54cb5561200
Adrian Conlon <adrian.conlon@gmail.com>
2026-05-19 20:15:10 +0100

    6502 single step tests converted to unit tests

---
+++ M6502/M6502.HarteTest/SingleStepTests.cs
@@ -0,0 +1,72 @@
+// <copyright file="SingleStepTests.cs" company="Adrian Conlon">
+// Copyright (c) Adrian Conlon. All rights reserved.
+// </copyright>
+namespace M6502.HarteTest
+{
+    using System.Reflection;
+
+    [TestClass]
+    public class SingleStepTests
+    {
+        private const string TestDataDirectory = @"C:\github\spectrum\libraries\EightBit\modules\65x02\6502\v1";
+        //private const string TestDataDirectory = @"C:\github\spectrum\libraries\EightBit\modules\65x02\wdc65c02\v1";
+
+        public static IEnumerable<object[]> OpcodeFiles
+        {
+            get
+            {
+                if (!Directory.Exists(TestDataDirectory))
+                {
+                    yield break;
+                }
+
+                foreach (var file in Directory.EnumerateFiles(TestDataDirectory, "*.json").OrderBy(f => f))
+                {
+                    if (new FileInfo(file).Length > 0)
+                    {
+                        yield return [file];
+                    }
+                }
+            }
+        }
+
+        public static string GetDisplayName(MethodInfo _, object[] data)
+        {
+            ArgumentNullException.ThrowIfNull(data);
+            return System.IO.Path.GetFileNameWithoutExtension((string)data[0]);
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(OpcodeFiles), DynamicDataSourceType.Property, DynamicDataDisplayName = nameof(GetDisplayName))]
+        public async Task Opcode_SingleStep(string filePath)
+        {
+            var runner = new TestRunner();
+            runner.Initialize();
+            var checker = new Checker(runner);
+            checker.Initialise();
+
+            using var suite = new OpcodeTestSuite(filePath);
+            await foreach (var test in suite.TestsAsync)
+            {
+                if (test is null)
+                {
+                    throw new InvalidOperationException("Test cannot be null");
+                }
+
+                checker.Check(test);
+
+                if (checker.Unimplemented)
+                {
+                    Assert.Inconclusive($"Opcode not implemented: {test.Name}");
+                    return;
+                }
+
+                if (checker.Invalid)
+                {
+                    Assert.Fail($"Test '{test.Name}' failed:{System.Environment.NewLine}{string.Join(System.Environment.NewLine, checker.Messages)}");
+                    return;
+                }
+            }
+        }
+    }
+}
--- M6502/M6502.HarteTest/Test.cs
+++ M6502/M6502.HarteTest/Test.cs
@@ -1,5 +1,6 @@
 ﻿namespace M6502.HarteTest
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "Instantiated by JSON deserializer")]
     internal sealed class Test
     {
         public string? Name { get; set; }
